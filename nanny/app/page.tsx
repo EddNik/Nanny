4dff4e528ae891334b96bee85fa69a9fda7e1962
Eduard <nikinna86@gmail.com>
2026-03-10 12:48:19 +0200

    add images

--- nanny/app/page.tsx
+++ nanny/app/page.tsx
@@ -7,41 +7,9 @@ import { useEffect, useState } from 'react';
 import { signInAnonymously, User } from 'firebase/auth';
 
 export default function Home() {
-  // const [user, setUser] = useState<User | null>(null);
-  // useEffect(() => {
-  //   console.log(signInAnonymously(auth));
-  //   signInAnonymously(auth)
-  //     .then((value) => {
-  //       console.log('Signed in user:', value.user);
-  //       setUser(value.user);
-  //     })
-  //     .catch((error) => {
-  //       console.error('Auth error:', error);
-  //     });
-
-  //   const babysittersRef = ref(db, 'babysitters');
-
-  //   get(babysittersRef).then((data) => {
-  //     if (data.exists()) {
-  //       console.log('Babysitters data:', data);
-  //     } else {
-  //       console.log('No data found');
-  //     }
-  //   });
-  // }, []);
-
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        {/* <h1>Firebase Test Page</h1>;
-        {user ? (
-          <p>
-            You are signed in as {user.email}: <strong>{user.uid}</strong>
-          </p>
-        ) : (
-          <p>Not signed in</p>
-        )} */}
-      </main>
+      <main className={styles.main}></main>
     </div>
   );
 }
