2623500bcd119cc1ee40c6766384b5de94c22057
Eduard <nikinna86@gmail.com>
2026-03-10 09:02:56 +0200

    test connection with firebase DB

---
+++ nanny/lib/firebase/firebase.ts
@@ -0,0 +1,24 @@
+import { FirebaseOptions, getApps, initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL || '',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '',
+};
+
+//to avoid re-initializing Firebase multiple times in Next.js or React.
+export const app = !getApps().length
+  ? initializeApp(firebaseConfig)
+  : getApps()[0];
+
+export const auth = getAuth(app);
+export const db = getDatabase(app);
+
+console.log('Auth:', auth);
+console.log('Database:', db);
